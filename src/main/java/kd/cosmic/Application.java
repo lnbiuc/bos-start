0e0a8e97d98918e0ec76911d3532c129a5253c70
lnbiuc <hi@lnbiuc.com>
2023-10-30 21:25:37 +0800

    start application

--- src/main/java/kd/cosmic/Application.java
+++ src/main/java/kd/cosmic/Application.java
@@ -1,5 +1,6 @@
 package kd.cosmic;
 
+import kd.bos.config.client.util.ConfigUtils;
 import kd.cosmic.server.Launcher;
 
 /**
@@ -16,7 +17,7 @@ public class Application {
 
         cosmic.setAppName("cosmic-ZZSLL-tb3XlXxx");
         cosmic.setWebPath("D:/Code/bos-server/webapp/static-file-service");
-
+        cosmic.setConfigUrl("127.0.0.1:2181", "zookeeper", "Dd112211");
         cosmic.setStartWithQing(false);
 
         cosmic.start();
